ec43fcc65fba17dde71f27078f648accbbaae035
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-02-22 23:07:49 +0100

    adds timer task

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import './styles.css';
+
+const refs = {
+  timer: document.querySelector('#timer-1'),
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.init();
+  }
+
+  init() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate - currentTime;
+      if (deltaTime < 0) {
+        refs.timer.innerHTML = "Time's up!";
+      }
+      const time = this.getTimeComponents(deltaTime);
+      this.updateClockface(time);
+    }, 1000);
+  }
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  }
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+  updateClockface({ days, hours, mins, secs }) {
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.mins.textContent = mins;
+    refs.secs.textContent = secs;
+  }
+}
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Feb 27, 2021'),
+});
